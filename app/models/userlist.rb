b241b26e1eb4c969897aabda602d6a6fe97019bb
Rejka <rejka@rejka.rej>
2025-04-16 11:05:11 +0700

    update userlist

--- app/models/userlist.rb
+++ app/models/userlist.rb
@@ -1,5 +1,15 @@
 class Userlist < ApplicationRecord
-    def self.ransackable_attributes(auth_object = nil)
-        [ "username" ]
-      end
+  def self.ransackable_attributes(auth_object = nil)
+    [ "username" ]
+  end
+
+  def self.table_columns
+    [
+      { label: "Username", field: "username", bold: true, type: "string" },
+      { label: "Email", field: "email", copiable: true, type: "email" },
+      { label: "Address", field: "address", bold: true, type: "string" },
+      { label: "Created At", field: "created_at", strftime: "%b %d, %Y ", form: false },
+      { label: "Updated At", field: "updated_at", strftime: "%b %d, %Y ", form: false }
+    ]
+  end
 end
